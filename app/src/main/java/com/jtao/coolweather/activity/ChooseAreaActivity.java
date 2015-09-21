6219e0bed135a583f4ae8303327f87d2038345eb
9Tao <jtaobox@126.com>
2015-09-21 21:17:43 +0800

    工具方法完毕，开始界面设计

--- app/src/main/java/com/jtao/coolweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/jtao/coolweather/activity/ChooseAreaActivity.java
@@ -4,8 +4,17 @@ import android.app.Activity;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.jtao.coolweather.R;
+import com.jtao.coolweather.db.CoolWeatherDB;
+import com.jtao.coolweather.model.City;
+import com.jtao.coolweather.model.Province;
 import com.jtao.coolweather.util.HttpUtil;
 import com.jtao.coolweather.util.LocationUtil;
 import com.jtao.coolweather.util.ObtainCitySet;
@@ -13,33 +22,73 @@ import com.jtao.coolweather.util.ObtainProvinceSet;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Tap on 2015/9/19.
  */
 public class ChooseAreaActivity extends Activity {
 
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+
+    //UI相关
+    private TextView tv_prompt;
+    private TextView tv_title;
+    private ListView lv_list;
+
+    /**
+     * 省列表
+     */
+    private List<Province> provinceList;
+    /**
+     * 市列表
+     */
+    private List<City> cityList;
+    /**
+     * 选中的省
+     */
+    private Province selectedProvince;
+    /**
+     * 选中的市
+     */
+    private City selectedCity;
+    /**
+     * 选中的级别
+     */
+    private int currentLevel;
+
+    /**
+     * ListView数据集合
+     */
+    private List<String> dataList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
     private MyHttpCallbackListener listener;
+
+    private CoolWeatherDB db;
 
     private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_choosearea);
 
-        listener = new MyHttpCallbackListener();
+        init();
 
         final LocationUtil lu = new LocationUtil();
 
         Location location = lu.requestLocation(this);
 
         ////////
-        new ObtainProvinceSet().execute("http://flash.weather.com.cn/wmaps/xml/china.xml");
-        new ObtainCitySet().execute("http://flash.weather.com.cn/wmaps/xml/shanxi.xml", "shanxi");
+        //new ObtainProvinceSet().execute("http://flash.weather.com.cn/wmaps/xml/china.xml");
+        //new ObtainCitySet().execute("http://flash.weather.com.cn/wmaps/xml/shanxi.xml", "shanxi");
         ///////
+
 
         try {
             str = URLEncoder.encode("晋城", "utf-8");
@@ -48,7 +97,7 @@ public class ChooseAreaActivity extends Activity {
         }
 
         if(location != null){
-            Log.d("ChooseAreaActivity", "----->直接获取位置信息");
+            //Log.d("ChooseAreaActivity", "----->直接获取位置信息");
 
             String address = getAddress(location2coordinate(location));
 
@@ -70,18 +119,66 @@ public class ChooseAreaActivity extends Activity {
 
     }
 
-    class MyHttpCallbackListener implements HttpCallbackListener{
+    /**
+     * 初始化
+     */
+    private void init() {
+        listener = new MyHttpCallbackListener();
+        tv_prompt = (TextView) findViewById(R.id.tv_prompt);
+        tv_title = (TextView) findViewById(R.id.tv_title);
+        lv_list = (ListView) findViewById(R.id.lv_list);
 
-        @Override
-        public void onFinish(String response) {
-            Log.d("ChooseAreaActivity", response);
-        }
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
 
-        @Override
-        public void onError(Exception e) {
+        lv_list.setAdapter(adapter);
 
+        db = CoolWeatherDB.getInstance(this);
+
+        lv_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        queryProvinces(); //启动加载省级数据
+    }
+
+
+    /**
+     * 查询全国所有省，优先从数据库中查，如果没有数据，则从接口获取
+     */
+    private void queryProvinces(){
+        provinceList = db.loadProvinces();
+        if(provinceList.size() >0){ //数据库中有数据
+            for(Province province : provinceList){
+                dataList.add(province.getQuName());
+            }
+            dataChanged("中国", LEVEL_PROVINCE);
+        } else {
+            new ObtainProvinceSet().execute("http://flash.weather.com.cn/wmaps/xml/china.xml");
+            provinceList = db.loadProvinces();
+            if(provinceList.size() > 0){
+                for(Province province : provinceList){
+                    dataList.add(province.getQuName());
+                }
+                dataChanged("中国", LEVEL_PROVINCE);
+            }
         }
     }
+
+    /**
+     * 通知adapter数据改变、设置title文本..
+     * @param title
+     * @param level
+     */
+    private void dataChanged(String title, int level) {
+        dataList.clear();
+        adapter.notifyDataSetChanged(); //通知数据集合改变
+        lv_list.setSelection(0); //将光标移置第一位
+        tv_title.setText(title);
+        currentLevel = level;
+    }
+
 
     /**
      * 通过location获取经纬度信息
@@ -107,5 +204,18 @@ public class ChooseAreaActivity extends Activity {
     Log.d("ChooseAreaActivity", "address--->" + address);
         return address;
 
+    }
+
+    class MyHttpCallbackListener implements HttpCallbackListener{
+
+        @Override
+        public void onFinish(String response) {
+            Log.d("ChooseAreaActivity", response);
+        }
+
+        @Override
+        public void onError(Exception e) {
+
+        }
     }
 }
